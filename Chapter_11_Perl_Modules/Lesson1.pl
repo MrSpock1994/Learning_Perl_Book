cec89ccea0904512049aead3611b4637ac8f1209
MrSpock1994 <willpokerpro2016@gmail.com>
2022-09-17 14:39:15 -0300

    Chapter about Modules

--- Chapter_11_Perl_Modules/Lesson1.pl
+++ Chapter_11_Perl_Modules/Lesson1.pl
@@ -22,3 +22,46 @@
 # The cpan command that comes with Perl can give you details on a module:
 # $ cpan -D Digest::SHA
 
+# Databases and DBI
+# The DBI (Database Interface) module doesn’t come with Perl, but it’s one of the most
+# popular modules since most people have to connect to a database of some sort. The
+# beauty of DBI is that it allows you to use the same interface for just about any database
+# server (or fake server, even), from simple comma-separated value files to enterprise
+# servers such as Oracle. It has ODBC drivers, and some of its drivers are even vendor-
+# supported. To get the full details, check out Programming the Perl DBI by Alligator
+# Descartes and Tim Bunce (O’Reilly). You can also take a look at the DBI website.
+# Once you install DBI, you also have to install a DBD (Database Driver). You can get a
+# long list of DBDs from MetaCPAN. Install the right one for your database server, and
+# ensure that you get the version that goes with the version of your server.
+# The DBI is an object-oriented module, but you don’t have to know everything about
+# OO programming to use it. You just have to follow the examples in the documenta‐
+# tion. To connect to a database, you use the DBI module, then call its connect method:
+# use DBI;
+# $dbh = DBI->connect($data_source, $username, $password);
+# The $data_source contains information particular to the DBD that you want to use,
+# so you’ll get that from the DBD. For PostgreSQL, the driver is DBD::Pg, and the
+# $data_source is something like:
+# my $data_source = "dbi:Pg:dbname=name_of_database";
+# Once you connect to the database, you go through a cycle of preparing, executing,
+# and reading queries:
+# my $sth = $dbh->prepare("SELECT * FROM foo WHERE bla");
+# $sth->execute();
+# my @row_ary = $sth->fetchrow_array;
+# $sth->finish;
+# When you are finished, you disconnect from the database:
+# $dbh->disconnect();
+# The DBI can do all sorts of other things too. See its documentation for more details.
+# Although it’s a bit old, Programming the Perl DBI is still mostly a good introduction to
+# the module.
+
+# The localtime Function
+# When you have a timestamp number (such as the ones from stat), it will typically
+# look something like 1454133253. That’s not very useful for most humans, unless you
+# need to compare two timestamps by subtracting. You may need to convert it to some‐
+# thing human-readable, such as a string like “Sat Jan 30 00:54:13 2016.” Perl can
+# do that with the localtime function in a scalar context:
+
+my $timestamp = 1454133253;
+my $date = localtime $timestamp;
+
+print "$date"
